454dfb04bc73f6f6502690c0ae3efc8339f2637c
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-30 22:00:22 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,8 +1,10 @@
-require 'pry'
 require 'nokogiri'
+require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  #binding.pry 
+  binding.pry
 end
+
+create_project_hash
